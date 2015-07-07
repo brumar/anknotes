4fc558e068213c82c9048ef576f673c16086af73
brumar <martin.bruno.mail@gmail.com>
2015-07-07 23:09:38 +0200

    add Missing files

--- anknotes_start.py
+++ anknotes_start.py
@@ -1,10 +1,1 @@
-<<<<<<< HEAD
-import os,sys,inspect
-
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"anknotes")))
-if cmd_subfolder not in sys.path:
-    sys.path.insert(0, cmd_subfolder)
-
-=======
->>>>>>> 155d40b1f21ee8336f1c8d81dbef09df4cb39236
 from anknotes import __main__
