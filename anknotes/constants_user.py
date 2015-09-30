c552a7e24c2dfe38aaf5bb9bed20511eca63506c
Avi Puchalapalli <git@puchalapalli.com>
2015-09-30 01:37:34 -0400

    Fix for user constants file

--- anknotes/constants_user.py
+++ anknotes/constants_user.py
@@ -1,9 +1,18 @@
+#!/usr/bin/env python
 # INSTRUCTIONS:
 # USE THIS FILE TO OVERRIDE THE MAIN SETTINGS FILE
 # PREFIX ALL SETTINGS WITH THE constants MODULE REFERENCE AS SHOWN BELOW:
 # DON'T FORGET TO REGENERATE ANY VARIABLES THAT DERIVE FROM THE ONES YOU ARE CHANGING
 
-from anknotes import constants
-# constants.EVERNOTE.API.IS_SANDBOXED = True
-# constants.SETTINGS.EVERNOTE.AUTH_TOKEN = 'anknotesEvernoteAuthToken_' + constants.EVERNOTE.API.CONSUMER_KEY + (
-		# "_SANDBOX" if constants.EVERNOTE.API.IS_SANDBOXED else "")
+try: from anknotes import constants 
+except:
+	import os
+	import imp 
+	path = os.path.dirname(__file__)
+	name = 'constants'
+	modfile, modpath, description = imp.find_module(name, [path + '\\'])	
+	constants=imp.load_module(name, modfile, modpath, description)
+	modfile.close()
+
+# constants.EVERNOTE.API.IS_SANDBOXED = True 
+# constants.SETTINGS.EVERNOTE.AUTH_TOKEN = 'anknotesEvernoteAuthToken_' + constants.EVERNOTE.API.CONSUMER_KEY + ("_SANDBOX" if constants.EVERNOTE.API.IS_SANDBOXED else "")				
