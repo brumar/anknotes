82ebece75573ba5bc6acd8b3d36c7dfd7f53841a
Avi Puchalapalli <git@puchalapalli.com>
2015-10-13 09:22:22 -0400

    Update create_subnotes logging

--- anknotes/create_subnotes.py
+++ anknotes/create_subnotes.py
@@ -26,13 +26,12 @@ from aqt.utils import getText
 
 def create_subnotes(guids):
     def create_subnote(guid):
-        def process_lists(note, lst, levels=None, names=None):
-            def get_log_fn():
-                return u'.'.join(map(str, levels)) + u' - ' + u'-'.join(names[1:])
-                
-            def add_log_entry(title, content, filename=None, prefix_content=True, **kw):                
+        def process_lists(note, lst, levels=None, names=None):                            
+            def add_log_entry(title, content, filename=None, prefix_content=True, title_pad=16, **kw):
+                names_padded = u''.join(map(lambda x: (x+':').ljust(33) + ' ', names[1:-1])) + names[-1]
                 fmts = dict(levels_pad=u'\t' * level, levels=u'.'.join(map(str, levels)),
-                            num_levels=len(levels), names=u': '.join(names).ljust(20))
+                            num_levels=len(levels), names=u': '.join(names[1:]).ljust(20),
+                            names_padded=names_padded)
                 fmts['levels_str'] = (fmts['levels'] + ':').ljust(6)
                 if prefix_content:
                     fmts['content'] = content
@@ -41,52 +40,13 @@ def create_subnotes(guids):
                     fmts['list_name'] = list_tag_names[lst_items.name]
                 content = fmt(content, 0, fmts)
                 if title:
-                    title = (fmt(title, 0, fmts) + u': ').ljust(16)
+                    title = (fmt(title, 0, fmts) + u': ').ljust(title_pad)
                 l.go(title + content, filename, **kw)
 
-            def check_subnote(li, sublist):
-                def check_heading_flags():
-                    if not isinstance(sublist.heading_flags, list):
-                        sublist.heading_flags = []
-                    for str_ in "`':":
-                        if sublist.heading.endswith(str_):
-                            sublist.heading_flags.append(str_)
-                            sublist.heading = sublist.heading[:-1*len(str_)]
-                            check_heading_flags()
-                            return
-
-                #Begin check_subnote()
-                if not (isinstance(li, Tag) and (li.name in list_tag_names) and li.contents and li.contents[0]):
-                    sublist.list_items.append(unicode(li))
-                    return sublist
-                sublist.heading = strip_tags(unicode(''.join(sublist.list_items)), True).strip()
-                sublist.is_reversible = not matches_list(sublist.heading, HEADINGS.NOT_REVERSIBLE) 
-                check_heading_flags()
-                if "`" in sublist.heading_flags:
-                    sublist.is_reversible = not sublist.is_reversible
-                sublist.use_descriptor = "'" in sublist.heading_flags or "`" in sublist.heading_flags
-                sublist.is_subnote = ':' in sublist.heading_flags or matches_list(sublist.heading, HEADINGS.TOP + '|' + HEADINGS.BOTTOM)
-                if not sublist.is_subnote:
-                    return sublist
-                sublist.subnote = li
-                return sublist
-
-            def add_note(sublist, new_levels, new_names):
-                subnote_html = unicode(sublist.subnote)
-                log_fn = u'..\\subnotes\\add_note*\\' + get_log_fn()
-                add_log_entry('SUBNOTE', '{levels_str} {names}: \n%s\n' % subnote_html, '..\\subnotes\\add_note', crosspost=log_fn, prefix_content=False)
-                myNotes.append([new_levels, new_names, subnote_html])
-
-            def process_list_item(contents):
-                sublist = DictCaseInsensitive(is_subnote=False, list_items=[])
-                for li in contents:
-                    sublist = check_subnote(li, sublist)
-                    if sublist.is_subnote:
-                        break
-                return sublist
-
             def process_tag():
-                def log_tag():
+                def get_log_fn():
+                    return u'.'.join(map(str, levels)) + u' - ' + u'-'.join(names[1:])
+                def log_tag():                    
                     if not lst_items.contents:
                         add_log_entry('NO TOP TEXT', conv_unicode(lst_items.contents), crosspost='no_top_text')
                     if lst_items.name in list_tag_names:
@@ -105,8 +65,52 @@ def create_subnotes(guids):
                             l.banner(title, '..\\subnotes\\process_tag')
                             create_subnote.logged_subnote = True
                         add_log_entry('SUBNOTE', sublist.heading)
-                        add_log_entry('', sublist.heading, '..\\subnotes\\process_tag', crosspost=[subnote_fn, subnote_shared])
-                        l.go(unicode(sublist.subnote), subnote_fn)                
+                        add_log_entry('', sublist.heading, '..\\subnotes\\process_tag', crosspost=subnote_fn)
+                        add_log_entry('{levels}', '{names_padded}', subnote_shared, prefix_content=False, title_pad=13)
+                        l.go(unicode(sublist.subnote), subnote_fn) 
+
+                def add_note(sublist, new_levels, new_names):
+                    subnote_html = unicode(sublist.subnote)
+                    log_fn = u'..\\subnotes\\add_note*\\' + get_log_fn()
+                    add_log_entry('SUBNOTE', '{levels_str} {names}: \n%s\n' % subnote_html, '..\\subnotes\\add_note', crosspost=log_fn, prefix_content=False)
+                    myNotes.append([new_levels, new_names, subnote_html])                
+                
+                def process_list_item(contents):
+                    def check_subnote(li, sublist):
+                        def check_heading_flags():
+                            if not isinstance(sublist.heading_flags, list):
+                                sublist.heading_flags = []
+                            for str_ in "`':":
+                                if sublist.heading.endswith(str_):
+                                    sublist.heading_flags.append(str_)
+                                    sublist.heading = sublist.heading[:-1*len(str_)]
+                                    check_heading_flags()
+                                    return
+
+                        #Begin check_subnote()
+                        if not (isinstance(li, Tag) and (li.name in list_tag_names) and li.contents and li.contents[0]):
+                            sublist.list_items.append(unicode(li))
+                            return sublist
+                        sublist.heading = strip_tags(unicode(''.join(sublist.list_items)), True).strip()
+                        sublist.base_title = u': '.join(names).replace(title + ': ', '')
+                        sublist.is_reversible = not matches_list(sublist.heading, HEADINGS.NOT_REVERSIBLE) 
+                        check_heading_flags()
+                        if "`" in sublist.heading_flags:
+                            sublist.is_reversible = not sublist.is_reversible
+                        sublist.use_descriptor = "'" in sublist.heading_flags or "`" in sublist.heading_flags
+                        sublist.is_subnote = ':' in sublist.heading_flags or matches_list(sublist.heading, HEADINGS.TOP + '|' + HEADINGS.BOTTOM)
+                        if not sublist.is_subnote:
+                            return sublist
+                        sublist.subnote = li
+                        return sublist                    
+                    
+                    # Begin process_list_item()
+                    sublist = DictCaseInsensitive(is_subnote=False, list_items=[])
+                    for li in contents:
+                        sublist = check_subnote(li, sublist)
+                        if sublist.is_subnote:
+                            break
+                    return sublist                        
                 
                 # Begin process_tag()                
                 new_levels = levels[:]
